eef13e9c02430b51af6ae8d63526879899c452fe
5648bean <k.isobe.gm@gmail.com>
2025-02-02 14:05:04 +0900

    Update insertMany.js

--- insertMany.js
+++ insertMany.js
@@ -1,7 +1,6 @@
 const { MongoClient } = require("mongodb");
 // 下のURIをご自分の値に変更してください
-const uri = "mongodb+srv://matsunami2:password@test.lfwohhl.mongodb.net/?retryWrites=true&w=majorityß";
-//const uri = "mongodb+srv://2201100184yh:V9xe;M!VfwTT!m&@cluster0.acmku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://2201100184yh:V9xe;M!VfwTT!m&@cluster0.acmku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 async function run() {
 const database = client.db('notes');
